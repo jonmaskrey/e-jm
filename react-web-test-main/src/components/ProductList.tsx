39211758e6f60cf6e75b0275dae2b4e5a650fea9
jmaskrey <jmaskrey@bmj.com>
2024-10-09 13:04:56 +0100

    added debounce/throttle

--- react-web-test-main/src/components/ProductList.tsx
+++ react-web-test-main/src/components/ProductList.tsx
@@ -1,16 +1,27 @@
 import { observer } from "mobx-react-lite";
 import { productStore } from "../stores/ProductStore";
+import useThrottle from "../hooks/useThrottle";
+import { useEffect, useState, useCallback } from "react";
 
 const ProductList = observer(() => {
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    productStore.setQuery(event.target.value);
-  };
+  const [searchTerm, setSearchTerm] = useState(productStore.query);
+  const throttledSearchTerm = useThrottle(searchTerm, 800);
+
+  useEffect(() => {
+    if (throttledSearchTerm !== productStore.query) {
+      productStore.setQuery(throttledSearchTerm);
+    }
+  }, [throttledSearchTerm]);
+
+  const handleSearch = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  }, []);
 
   return (
     <>
       <input
         type="text"
-        value={productStore.query}
+        value={searchTerm}
         onChange={handleSearch}
         placeholder="Search products..."
       />
--- react-web-test-main/src/stores/ProductStore.ts
+++ react-web-test-main/src/stores/ProductStore.ts
@@ -14,6 +14,7 @@ class ProductStore {
 
   constructor() {
     makeAutoObservable(this);
+    this.fetchProducts();
   }
 
   setQuery(query: string) {
